0f4164e7ed45dee259edb1f9e700b0fd26dfd288
Maximilian Schwarzmüller <company@academind.com>
2017-12-14 10:27:50 +0100

    finished http cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,94 @@
 const functions = require("firebase-functions");
-const gcs = require('@google-cloud/storage')();
-const os = require('os');
-const path = require('path');
-const spawn = require('child-process-promise').spawn;
+const os = require("os");
+const path = require("path");
+const spawn = require("child-process-promise").spawn;
+const cors = require("cors")({ origin: true });
+const Busboy = require("busboy");
+const fs = require("fs");
 
+const gcconfig = {
+  projectId: "fb-cloud-functions-demo",
+  keyFilename: "fb-cloud-functions-demo-firebase-adminsdk-km39q-405896eddb.json"
+};
+
+const gcs = require("@google-cloud/storage")(gcconfig);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange= functions.storage.object().onChange(event => {
-    const object = event.data;
-    const bucket = object.bucket;
-    const contentType = object.contentType;
-    const filePath = object.name;
-    console.log('File change detected, function execution started');
+exports.onFileChange = functions.storage.object().onChange(event => {
+  const object = event.data;
+  const bucket = object.bucket;
+  const contentType = object.contentType;
+  const filePath = object.name;
+  console.log("File change detected, function execution started");
 
-    if (object.resourceState === 'not_exists') {
-        console.log('We deleted a file, exit...');
-        return;
-    }
+  if (object.resourceState === "not_exists") {
+    console.log("We deleted a file, exit...");
+    return;
+  }
 
-    if (path.basename(filePath).startsWith('resized-')) {
-        console.log('We already renamed that file!');
-        return;
-    }
+  if (path.basename(filePath).startsWith("resized-")) {
+    console.log("We already renamed that file!");
+    return;
+  }
 
-    const destBucket = gcs.bucket(bucket);
-    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
-    const metadata = { contentType: contentType };
-    return destBucket.file(filePath).download({
-        destination: tmpFilePath
-    }).then(() => {
-        return spawn('convert', [tmpFilePath, '-resize', '500x500', tmpFilePath]);
-    }).then(() => {
-        return destBucket.upload(tmpFilePath, {
-            destination: 'resized-' + path.basename(filePath),
-            metadata: metadata
-        })
+  const destBucket = gcs.bucket(bucket);
+  const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+  const metadata = { contentType: contentType };
+  return destBucket
+    .file(filePath)
+    .download({
+      destination: tmpFilePath
+    })
+    .then(() => {
+      return spawn("convert", [tmpFilePath, "-resize", "500x500", tmpFilePath]);
+    })
+    .then(() => {
+      return destBucket.upload(tmpFilePath, {
+        destination: "resized-" + path.basename(filePath),
+        metadata: metadata
+      });
     });
+});
+
+exports.uploadFile = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (req.method !== "POST") {
+      return res.status(500).json({
+        message: "Not allowed"
+      });
+    }
+    const busboy = new Busboy({ headers: req.headers });
+    let uploadData = null;
+
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      const filepath = path.join(os.tmpdir(), filename);
+      uploadData = { file: filepath, type: mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on("finish", () => {
+      const bucket = gcs.bucket("fb-cloud-functions-demo.appspot.com");
+      bucket
+        .upload(uploadData.file, {
+          uploadType: "media",
+          metadata: {
+            metadata: {
+              contentType: uploadData.type
+            }
+          }
+        })
+        .then(() => {
+          res.status(200).json({
+            message: "It worked!"
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
+    busboy.end(req.rawBody);
+  });
 });
